6fba3264877777f40db2142d065db4bf23fae7bf
Aditi777613 <109480600+Aditi777613@users.noreply.github.com>
2024-06-30 13:20:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-west-2"  # Replace with your desired AWS region
+  region = "us-east-1"
 }
 
 resource "aws_instance" "strapi" {
-  ami           = "ami-04b70fa74e45c3917"
+  ami           = "ami-04b70fa74e45c3917"  
   instance_type = "t2.micro"
 
   user_data = <<-EOF
@@ -14,7 +14,7 @@ resource "aws_instance" "strapi" {
               sudo npm install -g strapi@latest
               mkdir -p /srv/strapi
               cd /srv/strapi
-              git clone https://github.com/your-username/your-strapi-repo.git .
+              git clone https://github.com/Aditi777613/strapi-ec2-terraform .
               npm install
               pm2 start npm --name "strapi" -- start
               pm2 save
@@ -22,31 +22,5 @@ resource "aws_instance" "strapi" {
 
   tags = {
     Name = "StrapiServer"
-  }
-}
-
-resource "aws_security_group" "strapi_sg" {
-  name        = "strapi_sg"
-  description = "Allow HTTP and SSH traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
